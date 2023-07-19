etcd-io/etcd

FSM.ApplyCommits(): new method
This is a step towards moving `ProcessCommits()` out of this interface
and into `raftNode`.

Signed-off-by: Michael Haggerty <mhagger@alum.mit.edu>

contrib/raftexample/raftexample_test.go
@@ -53,6 +53,10 @@ func (nullFSM) RestoreSnapshot(snapshot []byte) error {
 	return nil
 }
 
+func (nullFSM) ApplyCommits(commit *commit) error {
+	return nil
+}
+
 type cluster struct {
 	peerNames []string
 	peers     []*peer

mhagger/raft

raftexample/raftexample_test.go
@@ -53,6 +53,10 @@ func (nullFSM) RestoreSnapshot(_ []byte) error {
 	return nil
 }
 
+func (nullFSM) ApplyCommits(_ *commit) error {
+	return nil
+}
+
 type cluster struct {
 	peerNames []string
 	peers     []*peer